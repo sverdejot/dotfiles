3c55f6042159c5ad037c43c6315cbd3ca41c1219
sverdejot <sverdejot@gmail.com>
2023-12-29 16:14:42 +0100

    fix(macos): nerd fonts installing

--- os/osx/mac-os.sh
+++ os/osx/mac-os.sh
@@ -1,3 +1,7 @@
 # increase the default speed of arrow keys
 defaults write NSGlobalDomain InitialKeyRepeat -int 12
 defaults write NSGlobalDomain KeyRepeat -int 1ac
+
+# install nerd font for proper glyphs on nvim and alacritty
+brew tap homebrew/cask-fonts
+brew install font-jetbrains-mono-nerd-font
