45f5866c2d05f54ee7645666de0887c5de47277b
sverdejot <sverdejot@gmail.com>
2023-12-29 11:40:49 +0100

    feat(zsh): themes

---
+++ zsh/init.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/reobin/typewritten.git $ZSH_CUSTOM/themes/typewritten
+ln -s "$ZSH_CUSTOM/themes/typewritten/typewritten.zsh-theme" "$ZSH_CUSTOM/themes/typewritten.zsh-theme"
+ln -s "$ZSH_CUSTOM/themes/typewritten/async.zsh" "$ZSH_CUSTOM/themes/async"
+
