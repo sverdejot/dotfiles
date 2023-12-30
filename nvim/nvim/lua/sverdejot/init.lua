813709cf185b2cf792fae693c2e9ea98df8b80f9
sverdejot <sverdejot@gmail.com>
2023-12-30 03:23:39 +0100

    feat(nvim): some editorconfig

--- nvim/nvim/lua/sverdejot/init.lua
+++ nvim/nvim/lua/sverdejot/init.lua
@@ -7,3 +7,11 @@ vim.wo.relativenumber = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 
+-- searching
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
