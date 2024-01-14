92f097421c18be8eb47bedad7b49333c0f3512b2
sverdejot <sverdejot@gmail.com>
2024-01-14 13:57:22 +0100

    style(neovim): transparent background

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,3 +1,5 @@
 -- theme settings
 vim.cmd 'colorscheme gruvbox'
 vim.cmd 'set termguicolors'
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
