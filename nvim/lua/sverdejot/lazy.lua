5608c8170a114211fbef536f4b75ab5aad07018c
sverdejot <sverdejot@gmail.com>
2024-09-04 00:32:09 +0200

    feat: dap configuration

--- nvim/lua/sverdejot/lazy.lua
+++ nvim/lua/sverdejot/lazy.lua
@@ -44,6 +44,7 @@ dependencies = {
 		opts = {
 			ensure_installed = {
 				'gopls',
+				'templ',
 			}
 		}
 	},
@@ -190,4 +191,47 @@ dependencies = {
       require("refactoring").setup()
     end,
   },
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-neotest/nvim-nio",
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			local dap = require "dap"
+			local ui = require "dapui"
+
+			require("dapui").setup()
+			require("dap-go").setup()
+
+			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+			vim.keymap.set("n", "<space>?", function()
+				require("dapui").eval(nil, { enter = true})
+			end)
+
+			vim.keymap.set("n", "<space>cc", dap.continue)
+			vim.keymap.set("n", "<space>si", dap.step_into)
+			vim.keymap.set("n", "<space>so", dap.step_over)
+			vim.keymap.set("n", "<space>st", dap.step_out)
+			vim.keymap.set("n", "<space>sb", dap.step_back)
+			vim.keymap.set("n", "<space>sr", dap.restart)
+
+			dap.listeners.before.attach.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				ui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				ui.close()
+			end
+		end
+	},
 })
